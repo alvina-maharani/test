4483fbe91971527a21a0547af1574d2d81e97048
alvina-maharani <maharanialvina10@gmail.com>
2022-03-11 12:28:06 +0700

    menambahkan file hello.php

---
+++ hello.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>coba 1</title>
+</head>
+<body>
+    <h1>Nama : <?php echo "Alvina Maharani";?><h1> 
+    <h1>Nim : <?php echo "200411100029";?><h1>
+</body>
+</html>
